41c1c9eeddd258760baeb987c6a7175c69bf4894
colinteahan <24cteahan@xbhs.net>
2022-03-23 15:29:38 -0400

    update

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("js loaded")
-var url = document.URL
-
-var id = url.split("?")[1]
-
-if (id == "rr") {
-    document.getElementById("text").style.display = "none"
-    document.write('<video src="rolling.mp4"></video>')
-}
