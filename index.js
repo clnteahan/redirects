822793ce214bb91f4d19b1ce5d2b1461615e89eb
colinteahan <24cteahan@xbhs.net>
2022-03-23 14:56:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log("js loaded")
 var url = document.URL
 
-var splitUrl = url.split("?")
-console.log(splitUrl[1])
+var id = url.split("?")[1]
+
+if (id == "rr") {
+    console.log("rolled")
+    window.open("https://youtu.be/dQw4w9WgXcQ")
+}
